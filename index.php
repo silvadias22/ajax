892af8764a687ecfd05383be34c154f753b67800
Fabrício da Silva Dias <silvadias22@gmail.com>
2016-07-13 09:14:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php
-	
 	# Criado por - Fabrício da Silva Dias.
 	# silvadias22@gmail.com
 	# https://github.com/silvadias22
 	# https://gist.github.com/silvadias22
 
-
-	function conexaoPDO(){	
-		#Iniciando conexão com banco de dados
-		$pdo = new PDO ('mysql:host=localhost;dbname=ajax','root', '');
-		$pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-		return $pdo;
-	}
+	include ('getRegistros.php');
 ?>
-<!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta charset="utf8">
 		<link rel="stylesheet" href="css/bootstrap.css">
 		<link rel="stylesheet" href="css/style.css">
@@ -25,79 +16,83 @@
 		<div class="col-md-5 col-md-offset-4" id="conteudo">
 			<div class="panel panel-primary">
 				<div class="panel-heading">
-					<h3 class="panel-title text-center">Ajax</h3>
+					<h3 class="panel-title text-center">Ajax - Demonstração</h3>
 				</div>
 				<div class="panel-body">
 					<form class="col-md-6" action="#" name="#" method="post">
 						 <div class="form-group text-center">
-							<span>Selecione o registro:</span>
-							<select class="input-group form-control input-sm" id="registro" >
+							<span>Selecione um Estado:</span>
+							<select class="input-group form-control input-sm" id="estados" >
 								<?php 
 												
 									// atribui as configurações de conxão com o banco na variável $pdo através da função conexaoPDO();	
 									$pdo = conexaoPDO();
 
-									// Atribui na variável $item o preparo do select.
-									$item = $pdo->prepare("select Item_um from ajax;");
+									// Atribui na variável $estado o preparo do select.
+									$estado = $pdo->prepare("select uf from tb_estados;");
 
 									// Executa a variável.
-									$item->execute();
+									$estado->execute();
 
-									// Inicia um laço de repetição atribuindo na variável $itens os valores contido em $item,
+									// Inicia um laço de repetição atribuindo na variável $estados os valores contido em $estado,
 									// lembrando que fetch retorna a proxima linha do resultado setado.	
-									while($itens = $item->fetch(PDO::FETCH_ASSOC)){
+									while($estados = $estado->fetch(PDO::FETCH_ASSOC)){
 									
 									// Dentro do laço de repetição, a option do select é populado linha por linha.	
 								?>		
-								<option  name="registro" class="registro"><?php print_r($itens['Item_um']);?></option>
+								<option  name="estado" class="estado"><?php echo $estados['uf'];?></option>
 								<?php };?>
 							</select> 
 						</div> <!-- Div Form-Group -->
 					</form>
 					<div class="col-md-6 text-center" id="registroShow" >
-						<span>Registro Selecionado :</span>
-						<table class="table table-bordered table-striped table-condensed" style="margin-top:5px;" id="tab">
-							<tr class="success text-center" >
-								<td  id="registroSelected"></td>
-							</tr>
-						</table>
+						<span>Selecione uma cidade:</span>
+						<select class="input-group form-control input-sm" id="cidade" >
+						</select>
 					</div>	
 				</div>  <!-- Panel-Body -->
 			</div> <!-- Panel -->
-		</div> <!-- Div Conteúdo -->
+		</div> <!-- Div Conteúdo -->	
 		<script src="js/jquery.min.js"></script>
 		<script>
 			
 			//	Após o carregamento do documento, a função change é executada (Observe que o change é para o elemento)
 			//	Isso significa que após o carregamento da página, o elemento selecionado será executado. 
-			
+						
 			$(document).ready(function(){
-				$('#registro').change();
-
+				$('#estados').change();
 			});
 			
-		
 			//	Quando o elemento for alterado, o change executará o ajax,
 			//	Que fará uma requisição com destino em getRegistros.php utilizando o metodo 'POST',
-			//	enviando o valor armazenado na variável 'registro' (Valor selecionado no elemento #registro).
+			//	enviando o valor armazenado na varável 'uf' (Valor selecionado no elemento #estados).
+			//  Veja a continuação em getRegistros.php. 
 				
-			$("#registro").change( function(){ 
+			$("#estados").change( function(){ 
 				$.ajax({
-					url: "getRegistros.php",
-					dataType:"json",
-					type:"post",
+					type:"POST",
+					dataType: "json",
+					url:"getRegistros.php",
 					data:{
-						registro: $("#registro").val(),
+						uf : $("#estados").val()
 					},
 					
-					//	Aqui será recebido o retorno passado via JSON pelo gerRegistros.php,
-					//	em seguida, será limpado um local específico e por fim será enviado para esse local
-					//	a impressão do valor recebido.
 					
+					
+					beforeSend: function(){	
+						$("#registroSelected").empty();
+						$("#cidade").empty();
+					},
+
+					//	Aqui será recebido o retorno passado via JSON pelo getRegistros.php,
+					//	em seguida, será limpado um local específico e por fim será enviado para esse local(elemento 'Select')
+					//	uma option sob iteração do loop fazendo com que seja populado cada linha com o nome dos valores recebido.
+				
 					success: function(data){	
-						$("#registroSelected").empty();	
-						$('#registroSelected').append(data).hide().fadeIn( 'fast' , 'linear' );
-						$("#registroSelected").show();
+						$.each(data, function(key, value){
+							//console.log(value);
+							$("#cidade").append("<option >"+value.nome+"</option>");
+						});	
 					}						
 				});   
 			 });
